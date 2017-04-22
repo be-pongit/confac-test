0edaea998316f0a5f3c4d0d3c08a131388b175d9
unknown <hvans@.(none)>
2017-04-22 22:54:06 +0200

    initial commit

---
+++ test1.rb
@@ -0,0 +1,17 @@
+require "selenium-webdriver"
+
+#Opens in Firefox
+driver = Selenium::WebDriver.for :firefox
+driver.navigate.to "http://pongit:7000"
+
+#Homepage
+klantenbtn = driver.find_element(:css, "[data-tst='link-clients']")
+klantenbtn.click
+
+#Klanten Page
+nieuweklantbtn = driver.find_element(:css, "[data-tst='new-client']")
+nieuweklantbtn.click
+
+#Nieuwe Klant Page
+bedrijfsnaambtn = driver.find_element(:css, "[data-tst='config.company.name']")
+bedrijfsnaambtn.send_keys("De Pennelekker")
